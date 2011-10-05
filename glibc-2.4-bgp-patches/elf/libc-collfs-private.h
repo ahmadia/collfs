b9958158f1b55fd683e486144b2c429cb9c43a01
Jed Brown <jed@59A2.org>
2011-10-05 17:57:24 -0500

    Add intercept capability to libc-collfs-private.h

--- glibc-2.4-bgp-patches/elf/libc-collfs-private.h
+++ glibc-2.4-bgp-patches/elf/libc-collfs-private.h
@@ -15,4 +15,15 @@ int __collfs_open(const char *pathname, int flags, ...);
 int __collfs_close(int fd);
 ssize_t __collfs_read(int fd, void *buf, size_t count);
 
+#ifdef COLLFS_INTERCEPT
+#  define __fxstat64(vers, fd, buf)                   __collfs_fxstat64(vers, fd, buf)
+#  define __xstat64(vers, file, buf)                  __collfs_xstat64(vers, file, buf)
+#  define __mmap(addr, len, prot, flags, fildes, off) __collfs_mmap(addr, len, prot, flags, fildes, off)
+#  define __unmap(addr, len)                          __collfs_munmap(addr,len)
+#  define __lseek(fildes, offset, whence)             __collfs_lseek(fildes, offset, whence)
+#  define __open(pathname,flags)                      __collfs_open(pathname,flags)
+#  define __close(fd)                                 __collfs_close(fd)
+#  define __libc_read(fd, buf, count)                 __collfs_read(fd, buf, count)
+#endif
+
 #endif
