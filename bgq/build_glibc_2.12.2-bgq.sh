52a5ecd1a8d1791872b722d3d2b25164e9b8d850
William Scullin <wscullin@alcf.anl.gov>
2012-04-27 09:19:50 +0000

    Build script for bgq now can be run with fewer changes on an ANL /Q

--- bgq/build_glibc_2.12.2-bgq.sh
+++ bgq/build_glibc_2.12.2-bgq.sh
@@ -2,7 +2,7 @@
 collfs_dir_rel=`dirname $0`/..
 collfs_dir=`readlink -f $collfs_dir_rel`
 tmpdir=`pwd`
-prefix=/home/aron/bgqsys
+prefix=/home/$USER/bgqsys
 build=powerpc64-linux-gnu
 
 glibc_src_dir=$tmpdir/glibc-2.12.2
@@ -16,8 +16,8 @@ cd $tmpdir
 #curl -O ftp://ftp.gnu.org/gnu/glibc/glibc-2.12.2.tar.gz
 #tar -zxvf glibc-2.12.2.tar.gz
 #or
-### rm -rf glibc-2.12.2
-### tar -xvf /opt/share/downloads/glibc-2.12.2.tar.gz
+rm -rf glibc-2.12.2
+tar -zxvf /bgsys/downloads/glibc-2.12.2.tar.gz
 #endif 
 
 # apply IBM patches to glibc
@@ -25,7 +25,7 @@ cd $tmpdir
 # you may need the latest driver RPM from http://wiki.bg.anl-external.org/index.php/Main_Page
 # specifically http://bg-toolchain.anl-external.org/wiki/images/7/78/BgptoolchainSA_V1R4M2-2010.1.4-2.ppc64.rpm
 
-### patch -p2 -E < /bgsys/drivers/ppcfloor/toolchain/glibc-2.12.2.diff
+patch -d glibc-2.12.2 -p0 -E < /bgsys/drivers/ppcfloor/toolchain/glibc-2.12.2.diff
 
 # temporary linux headers
 #mkdir -p $tmpdir/templinuxheaders-build/include && \
@@ -39,17 +39,17 @@ cd $tmpdir
 #        $tmpdir/templinuxheaders-build/include/linux
 
 # patch makeconfig
-### cp $collfs_dir/glibc-2.12.2-bgq-patches/Makeconfig $glibc_src_dir
+cp $collfs_dir/glibc-2.12.2-bgq-patches/Makeconfig $glibc_src_dir
 
 # apply collfs patches
-### cp $collfs_dir/glibc-2.12.2-bgq-patches/elf/* $glibc_src_dir/elf
+cp $collfs_dir/glibc-2.12.2-bgq-patches/elf/* $glibc_src_dir/elf
 
 # configure using default-shared gcc specs
 rm -rf $tmpdir/glibc-2.12.2-build
 mkdir -p $tmpdir/glibc-2.12.2-build
 cd $tmpdir/glibc-2.12.2-build &&  \
-    PATH=/usr/gnu/bin:/usr/bin:/bin:/bgsys/drivers/toolchain/V1R1M0-2012_0131_2135/gnu-linux/bin/ LD_LIBRARY_PATH=    \
-    CC="powerpc64-bgq-linux-gcc -g -specs=/bgsys/drivers/toolchain/V1R1M0-2012_0131_2135/gnu-linux/lib/gcc/powerpc64-bgq-linux/4.4.6/specs.orig" \
+    PATH=/usr/gnu/bin:/usr/bin:/bin:/bgsys/drivers/toolchain/V1R1M0-/gnu-linux/powerpc64-bgq-linux/bin:/bgsys/drivers/V1R1M0/ppc64/gnu-linux/bin LD_LIBRARY_PATH=    \
+    CC="powerpc64-bgq-linux-gcc -g -lgcc_eh -lgcc -specs=/bgsys/drivers/toolchain/V1R1M0-/gnu-linux/lib/gcc/powerpc64-bgq-linux/4.4.6/specs.orig" \
     libc_cv_ppc_machine=yes  \
     libc_cv_forced_unwind=yes  \
     libc_cv_c_cleanup=yes  \
@@ -66,7 +66,7 @@ cd $tmpdir/glibc-2.12.2-build &&  \
     --host=powerpc64-bgq-linux   \
     --enable-shared  \
     --enable-static-nss \
-    --enable-multilib \
+    --disable-multilib \
     --without-cvs \
     --without-gd \
     --with-elf \
@@ -75,12 +75,13 @@ cd $tmpdir/glibc-2.12.2-build &&  \
     --with-tls \
     --with-__thread   \
     --enable-__cxa_atexit \
-    --with-headers=/bgsys/drivers/ppcfloor/linux/include/
+    --with-headers=/bgsys/drivers/V1R1M0/ppc64/toolchain/gnu/build-powerpc64-bgq-linux/templinuxheaders-build/include \
+    --with-binutils=/bgsys/drivers/toolchain/V1R1M0-/gnu-linux/powerpc64-bgq-linux/bin
 
 # make
-cd $tmpdir/glibc-2.12.2-build && PATH=/bgsys/drivers/toolchain/V1R1M0-2012_0131_2135/gnu-linux/bin/ LD_LIBRARY_PATH= \
-    make 2>&1 | tee make.log
+cd $tmpdir/glibc-2.12.2-build && PATH=/bin:/usr/bin:/bgsys/drivers/V1R1M0/ppc64/gnu-linux/bin:/bgsys/drivers/toolchain/V1R1M0-/gnu-linux/powerpc64-bgq-linux/bin LD_LIBRARY_PATH= \
+   make -j4 2>&1 | tee make.log
 
 # make install
-cd $tmpdir/glibc-2.12.2-build && PATH=/bgsys/drivers/toolchain/V1R1M0-2012_0131_2135/gnu-linux/bin/ LD_LIBRARY_PATH= \
-    make install 2>&1 | tee install.log
+cd $tmpdir/glibc-2.12.2-build && PATH=/bgsys/drivers/toolchain/V1R1M0-/gnu-linux/bin:/bgsys/drivers/toolchain/V1R1M0-/gnu-linux/powerpc64-bgq-linux/bin:/bin:/usr/bin LD_LIBRARY_PATH= \
+   make install 2>&1 | tee install.log
