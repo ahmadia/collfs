5a2676de441a19c2f4d8e7fa422d46f3ba7e5ef7
Jed Brown <jed@59A2.org>
2012-04-28 14:10:07 +0000

    Forgot to update minimal_thefunc. Be sure to build libminimal_thefunc.so before running the test in the last commit

--- minimal_thefunc.c
+++ minimal_thefunc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
-int thefunc(void) {
-  return printf("called %s\n", __func__) <= 0;
+int thefunc(int verbosity) {
+  if (verbosity >= 0) return printf("called %s\n", __func__) <= 0;
+  else return 0;
 }
