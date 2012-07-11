2ac85c96e046783bebd34217b4ffc8dfb850895f
Aron Ahmadia <aron@ahmadia.net>
2012-07-11 18:12:50 -0400

    added sync before internal import benchmark for python

--- test_python_importer.py
+++ test_python_importer.py
@@ -1,10 +1,15 @@
-from mpi4py import MPI
 import time
+full_start = time.time()
 
+from mpi4py import MPI
+
+MPI.COMM_WORLD.Barrier()
 t = time.time()
 from clawpack import pyclaw
 MPI.COMM_WORLD.Barrier()
 elapsed = time.time() - t
+full_elapsed = time.time() - full_start
 
 if (MPI.COMM_WORLD.rank == 0):
-    print "elapsed time: %f" % elapsed
+    print "elapsed time (import): %f" % elapsed
+    print "elapsed time (full): %f" % full_elapsed
